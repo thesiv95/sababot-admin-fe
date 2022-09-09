f83bca7eeb9a5e2134e2f7f86ba5bb028232384f
Ilya Soloveychik <dj.active.17@gmail.com>
2022-09-09 23:46:05 +0300

    refactor: some more consts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,10 @@
 export const DELAY_API_REQUEST_MS = 1000;
 export const LOADING =
   "База данных пуста - попробуйте восстановить данные (кнопка ниже) или дождитесь загрузки";
-export const NO_WORDS_BY_QUERY = "Нет слов по запросу %s в базе";
+export const NO_ITEMS_BY_QUERY = "Нет элементов по запросу %s в базе";
+export const NO_DATA_ON_PAGE = "На странице %d данных уже нет!";
+export const ITEM_INSERTED = "Успешно добавлено в базу";
+export const ITEM_MODIFIED = "Успешно изменено в базе";
+export const ITEM_DELETED = "Успешно удалено из базы";
+export const ITEMS_RESTORED = "Все %s восстановлены из резервной копии (обновите страницу)";
+export const ERROR500 = "Произошла ошибка";
