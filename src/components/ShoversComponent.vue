598fe596f7691e6b183c16ce0702551684306afb
Ilya Soloveychik <dj.active.17@gmail.com>
2022-09-09 22:23:02 +0300

    fix: loading text as const, input refactor

--- src/components/ShoversComponent.vue
+++ src/components/ShoversComponent.vue
@@ -3,7 +3,8 @@ import * as Api from "@/utils/api";
 import * as GuiModify from "@/utils/guiMod";
 import getToast from "@/utils/getToast";
 import showConfirmation from "@/utils/showConfirmation";
-import { DELAY_API_REQUEST_MS } from "@/utils/consts";
+import getCommonInput from "./plainHTMLs/getCommonInput";
+import { DELAY_API_REQUEST_MS, LOADING } from "@/utils/consts";
 
 export default {
   methods: {
@@ -63,14 +64,9 @@ export default {
         event.path[2].children[3].innerText,
       ];
 
-      const [he, translit, ru] = currentData;
-
       const { value: formValues } = await this.$swal.fire({
         title: "Изменить",
-        html:
-          `<input id="ru" placeholder="на русском" class="swal2-input" value="${ru}">` +
-          `<input id="translit" placeholder="транскрипция" class="swal2-input" value="${translit}">` +
-          `<input id="he" placeholder="на иврите" class="swal2-input" value="${he}">`,
+        html: getCommonInput(currentData),
         focusConfirm: false,
         preConfirm: () => {
           return [
@@ -150,10 +146,7 @@ export default {
 
       const { value: formValues } = await this.$swal.fire({
         title: "Добавить",
-        html:
-          '<input id="ru" placeholder="на русском" class="swal2-input">' +
-          '<input id="translit" placeholder="транскрипция" class="swal2-input">' +
-          '<input id="he" placeholder="на иврите" class="swal2-input">',
+        html: getCommonInput(),
         focusConfirm: false,
         preConfirm: () => {
           return [
@@ -244,6 +237,7 @@ export default {
   data() {
     return {
       loading: true,
+      loadingText: LOADING,
       items: null,
     };
   },
@@ -285,8 +279,7 @@ export default {
       </tbody>
     </table>
     <div v-else style="margin: 100px 0; color: black; font-size: 24px">
-      База данных пуста - попробуйте восстановить данные (кнопка ниже) или
-      дождитесь загрузки
+      {{ this.loadingText }}
     </div>
   </div>
   <div>
